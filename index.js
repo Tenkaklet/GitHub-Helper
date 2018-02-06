bb4492d1d1b6a7de4110b172e003bbb0c418ee1f
Max Carlquist <tenkakle@gmail.com>
2018-02-06 22:50:20 +0100

    updated index / begun github implementation

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
-
+const github = require('./lib/github');
 const inquirer = require('./lib/inquirer');
 
 const run = async () => {
-  const credentials = await inquirer.askGitHubCredentials();
-  console.log(credentials);
+  let token = github.getStoredGithubToken();
+  if(!token) {
+    await github.getStoredGithubToken();
+    token = await github.registerNewToken();
+  }
+  console.log(token);
 }
 
 clear();
+
 console.log(
-    chalk.yellow(
-      figlet.textSync('GitHub Helper', { horizontalLayout: 'full' })
-    )
-  );
+  chalk.yellow(
+    figlet.textSync('GitHub Helper', { horizontalLayout: 'full' })
+  )
+);
 
-  if (files.directoryExists('.git')) {
-    console.log(chalk.red('Already a Git Repository'));
-    process.exit();
-  }
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a Git Repository'));
+  process.exit();
+}
 
 
-  run();
+run();
